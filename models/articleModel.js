f1a75278265b88004362b8af3f3810c9096efd55
Anish Sinha <anishsinha0128@gmail.com>
2021-07-22 14:02:54 -0400

    Created schema for article

--- models/articleModel.js
+++ models/articleModel.js
@@ -1,27 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose"); //Import mongoose
+const slugify = require("slugify"); //Import slugify
 
-const articleSchema = new mongoose.Schema({
+const articleSchema = new mongoose.Schema( //Initialize new schema
+  {
     name: {
-        type: String,
-        required: [true, 'Article name required'],
+      type: String,
+      required: [true, "Article name required"],
     },
     content: {
-        type: String,
-        required: [true, 'Article must have body'],
+      type: String,
+      required: [true, "Article must have content"],
     },
     date: {
-        type: Date,
-        default: Date.now(),
-    }, 
-    author: {
-        type: String,
-        required: [true, 'Article must have author'],
+      type: Date,
+      required: [true, "Article must have a date"],
+      default: Date.now(),
     },
-    status: {
-        type: Boolean,
-        default: false,
-    }
-})
+    author: {
+      type: String,
+      required: [true, "Article must have author"],
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    slug: {
+      type: String,
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
-const Article = mongoose.model('Article', articleSchema);
-module.exports = Article;
+articleSchema.pre("save", function (next) {
+  //Before saving, create a slug
+  this.slug = slugify(this.name, {
+    lower: true, //make the slug lowercase
+  });
+  next(); //call next middleware
+});
+
+const Article = mongoose.model("Article", articleSchema); //Model schema
+module.exports = Article; //Export schema

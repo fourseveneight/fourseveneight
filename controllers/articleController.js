d83fdcd37cc7589589a1abd9be305f62da245b51
Anish Sinha <anishsinha0128@gmail.com>
2021-07-22 14:03:48 -0400

    Created file to perform CRUD operations when invoked in routes

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,4 +1,10 @@
-const factory = require('./handlerFactory');
-const Article = require('../models/articleModel');
+const factory = require("./handlerFactory"); //Import factory functions
+const Article = require("../models/articleModel"); //Import necessary model
 
-exports.getAllArticles = factory.getAll(Article);
+//CRUD functionality
+
+exports.createArticle = factory.createOne(Article); //? Create
+exports.getAllArticles = factory.getAll(Article); //? Read
+exports.getArticle = factory.getOne(Article); //? Read
+exports.updateArticle = factory.updateOne(Article); //? Update
+exports.deleteArticle = factory.deleteOne(Article); //? Delete

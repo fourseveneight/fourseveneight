bd9173f7258d1ae8bfa7f1767be0b8487fac1163
Anish Sinha <anishsinha0128@gmail.com>
2021-07-22 14:01:19 -0400

    Implemented routes for CRUD operations

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,8 +1,17 @@
-const express = require('express');
-const articleController = require('../controllers/articleController');
+const express = require("express"); //Import express
+const articleController = require("../controllers/articleController"); //Import necessary controllers
 
-const router = express.Router();
+const router = express.Router(); //Initialize express router
 
-router.route('/').get(articleController.getAllArticles);
+router
+  .route("/") //Route on /api/v1/articles/
+  .get(articleController.getAllArticles) //get all articles
+  .post(articleController.createArticle); //create new article
 
-module.exports = router;
+router
+  .route("/:slug") //Make slug a query parameter
+  .get(articleController.getArticle) //get article
+  .patch(articleController.updateArticle) //update article
+  .delete(articleController.deleteArticle); //delete article
+
+module.exports = router; //export router for use in app.js

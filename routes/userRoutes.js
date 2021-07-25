294df2bfe1d86cc5d7b5f7e77b9ebeaf14b1203b
Anish Sinha <anishsinha0128@gmail.com>
2021-07-24 21:24:55 -0400

    added routes for login/register/logout

---
+++ routes/userRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express'); //require express
+const userController = require('../controllers/userController'); //import controllers
+
+const router = express.Router(); //initialize router
+
+router
+  .route('/login') //on /login
+  .get((req, res) => {
+    res.status(200).json({
+      //just send this JSON for now
+      status: 'success',
+      data: {
+        message: 'route works',
+      },
+    });
+  })
+  .post(userController.login); //login function
+
+router
+  .route('/register')
+  .get((req, res) => {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        message: 'route also works',
+      },
+    });
+  })
+  .post(userController.register);
+
+router.get('/logout', userController.logout);
+
+module.exports = router;

1d061fc61188ab8c1305b9078261e0a056eac4a5
Anish Sinha <anishsinha0128@gmail.com>
2021-07-25 13:51:13 -0400

    Added functionality for password resets

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,18 +2,16 @@ const passport = require('passport'); //get passport
 const bcrypt = require('bcryptjs');
 const factory = require('./handlerFactory'); //import factory
 const User = require('../models/userModel'); //import user model
-const sendEmail = require('../util/email');
-const { createResetToken } = require('../auth/auth');
+const sendEmail = require('../util/email'); //Import email send utility
+const { createResetToken } = require('../auth/auth'); //Import createResetToken from auth
 
+//TODO: Protect these routes
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
 
-// exports.login = (req, res, next) => {
-//   passport.authenticate('local');
-// };
-
+//*This route needs to be modified once a proper frontend is built.
 exports.login = (req, res, next) => {
   passport.authenticate('local', {
     successRedirect: '/success',
@@ -114,39 +112,10 @@ exports.logout = (req, res, next) => {
   });
 };
 
-exports.forgotPassword = (req, res, next) => {
-  //User is stored in req.user IF logged in, so if req.user, we don't need to get their email address since we already know it
-  if (req.user) {
-    sendEmail({
-      email: req.user.email, //Send email to address associated with currently logged in account
-      subject: 'Password change link',
-      message: 'password-reset-token-goes-here',
-    });
-    res.status(200).json({
-      //Send JSON to verify it worked (will be changed in production)
-      status: 'success',
-      data: {
-        message: 'Check the email associated with this account.',
-      },
-    });
-  }
-  sendEmail({
-    email: req.body.email, //Send email to address found in req.body, since the user associated with this account is not logged in
-    subject: 'Password change link',
-    message: 'password-reset-token-goes-here',
-  });
-  res.status(200).json({
-    //Send JSON to verify it worked (will be changed in production)
-    status: 'success',
-    data: {
-      message: 'Check the inbox of the email you entered',
-    },
-  });
-};
-
-exports.changePassword = (req, res, next) => {
-  const user = User.findOne({ email: req.body.email });
+exports.forgotPassword = async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
   if (!user) {
+    //If there is no user, return an error
     res.status(404).json({
       status: 'failed',
       data: {
@@ -154,5 +123,80 @@ exports.changePassword = (req, res, next) => {
       },
     });
   }
-  const token = createResetToken();
+  const token = createResetToken(); //Create a token with two of the necessary parts
+  token.push(user.id); //Add the third part
+  sendEmail({
+    email: req.body.email, //Send email to the email passed into the body
+    subject: 'Password change link',
+    message: `localhost:8000/api/v1/users/change-password/${token[0]}/${token[1]}/${token[2]} is your password reset link`,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      message: 'successfully sent email with reset token!',
+    },
+  });
+};
+
+exports.changePassword = (req, res, next) => {
+  const { password, passwordConfirm } = req.body; //split req.body into two variables
+  const errors = [];
+  //Do password validation
+  if (password !== passwordConfirm) {
+    errors.push({ msg: 'passwords do not match!' });
+  }
+  if (password.length < 8) {
+    errors.push({ msg: 'new password length cannot be below 8 characters!' });
+  }
+  if (errors.length > 0) {
+    res.status(400).json({
+      status: 'failed',
+      data: {
+        errors,
+      },
+    });
+  }
+  User.findById({ _id: req.params.id }).then((user) => {
+    //Find the user by the ObjectId in req.params (3rd part of the token)
+    if (!user) {
+      //If this is unsuccessful:
+      res.status(400).json({
+        status: 'failed',
+        data: {
+          message: 'malformed reset token!',
+        },
+      });
+    }
+    //Otherwise, generate a salted and hashed password, and replace the user's password with this
+    bcrypt.genSalt(10, (err, salt) => {
+      //Generate a salt
+      // eslint-disable-next-line no-shadow
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        //Then a hash. Our password is now successfully salted and hashed
+        if (err) throw err;
+        user.password = undefined; //set it to undefined first, for extra security
+        user.password = hash; //then set it to the new salted and hashed password
+        user
+          .save() //Save the user in the database
+          .then(() => {
+            //then send a success message (just a json for now)
+            res.status(200).json({
+              status: 'success',
+              data: {
+                message: 'password successfully changed!',
+              },
+            });
+          })
+          .catch(() => {
+            //if any error happened after all that jank, send a 500 internal server error
+            res.status(500).json({
+              status: 'failed',
+              data: {
+                message: 'internal server error!',
+              },
+            });
+          });
+      });
+    });
+  });
 };

f42be7721dc90e86f0be90b9662e9888b05cf813
Anish Sinha <anishsinha0128@gmail.com>
2021-07-24 21:27:17 -0400

    Modified app.js and server.js. Will change this soon

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require("express"); //Import express
+// const express = require('express'); //Import express
 
-const articleRouter = require("./routes/articleRouter"); //require article router for /api/v1/articles routes
+// const articleRouter = require('./routes/articleRoutes'); //require article router for /api/v1/articles routes
+// const userRouter = require('./routes/userRoutes');
 
-const app = express(); //Initialize application
-app.use(express.json()); //Use express.json() to parse request body
+// const app = express(); //Initialize application
+// app.use(express.json()); //Use express.json() to parse request body
+// app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/v1/articles", articleRouter); //Use articleRouter on /api/v1/articles calls
+// app.use('/api/v1/articles', articleRouter); //Use articleRouter on /api/v1/articles calls
+// app.use('/api/v1/users', userRouter);
 
-module.exports = app; //Export app for use in server.js
+// module.exports = app; //Export app for use in server.js
--- server.js
+++ server.js
@@ -1,18 +1,36 @@
-const mongoose = require("mongoose"); //Import mongoose
-const dotenv = require("dotenv"); //Require dotenv module
+const dotenv = require('dotenv'); //Require dotenv module
 
-process.on("uncaughtException", (err) => {
+dotenv.config({ path: `./config/config.env` }); //Configure path for environment variables
+const express = require('express'); //Import express
+const mongoose = require('mongoose'); //Import mongoose
+
+const session = require('express-session'); //import session
+const passport = require('passport'); //import passport
+require('./auth/passportConfig')(passport); //require file
+
+const app = express(); //Initialize application
+const port = process.env.PORT || 8000; //Define port as the port listed in ./config/config.env or 8000
+app.use(express.json()); //Use express.json() to parse request body
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: `${process.env.SESSION_SECRET}`, // use session secret in config.env
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize()); //use these two middleware functions from passport
+app.use(passport.session());
+
+process.on('uncaughtException', () => {
   //On uncaught exception, exit
   process.exit(1);
 });
 
-dotenv.config({ path: `./config/config.env` }); //Configure path for environment variables
-
-const app = require("./app"); //Import app from app.js
 const currentTime = new Date(); //Set current time
 
 const DB = process.env.DATABASE.replace(
-  "<password>",
+  '<password>',
   process.env.DATABASE_PASSWORD
 ); //Initialize DB variable by replacing the password field in the string with the environment variable DATABASE_PASSWORD
 
@@ -25,19 +43,26 @@ mongoose //connect DB
     useUnifiedTopology: true,
   })
   .then(() => {
+    // eslint-disable-next-line no-console
     console.log(
       `Database connection successful as of ${currentTime.toLocaleString()}` //On save, verify connection with database with current timestamp
     );
   })
   .catch((err) => {
+    // eslint-disable-next-line no-console
     console.log(err);
   });
+const articleRouter = require('./routes/articleRoutes'); //require article router for /api/v1/articles routes
+const userRouter = require('./routes/userRoutes');
 
-const port = process.env.PORT || 8000; //Define port as the port listed in ./config/config.env or 8000
+app.use('/api/v1/articles', articleRouter); //Use articleRouter on /api/v1/articles calls
+app.use('/api/v1/users', userRouter);
+
 const server = app.listen(port); //Start server on port
 
-process.on("unhandledRejection", (err) => {
+process.on('unhandledRejection', (err) => {
   //On unhandled rejection, exit process
+  // eslint-disable-next-line no-console
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);

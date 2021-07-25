082480288c82d52f8d08101c906acc01f174cc7e
Anish Sinha <anishsinha0128@gmail.com>
2021-07-25 17:32:35 -0400

    Added function to auth.js

--- auth/auth.js
+++ auth/auth.js
@@ -1,3 +1,11 @@
+/*************************************************
+ *
+ * *Functions for verifying authentication status
+ * *and restricting routes
+ *
+ * **********************************************/
+const crypto = require('crypto');
+
 module.exports = {
   verifyAuthenticated: function (req, res, next) {
     if (req.isAuthenticated()) {
@@ -9,5 +17,25 @@ module.exports = {
         message: 'unable to authorize',
       },
     });
+  },
+  restrictTo: function (req, res, next) {
+    if (req.user.role === 'admin' || req.user.role === 'root') {
+      return next();
+    }
+    res.status(403).json({
+      status: 'failed',
+      data: {
+        message: 'unable to authorize',
+      },
+    });
+  },
+  //Function to create password reset token
+  createResetToken: function () {
+    const token = [];
+    const timestamp = Date.now();
+    const hash = crypto.randomBytes(20).toString('hex'); //random id
+    token.push(timestamp);
+    token.push(hash);
+    return token;
   },
 };

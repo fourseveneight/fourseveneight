7ca56028829be197c8076b0172a137169dd5c86e
Anish Sinha <anishsinha0128@gmail.com>
2021-07-24 21:21:47 -0400

    Updated handler factory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -5,60 +5,67 @@
  *
  *****************************************************/
 
-exports.getAll = (Model) => async (req, res, next) => {
-  const doc = await Model.find();
-  res.status(200).json({
-    status: "success",
-    results: doc.length,
-    data: {
-      data: doc,
-    },
-  });
-};
+const catchAsync = require('../util/catchAsync');
 
-exports.getOne = (Model) => async (req, res, next) => {
-  let query = Model.find({ slug: req.params.slug });
-  const doc = await query;
-  res.status(200).json({
-    status: "success",
-    data: {
-      data: doc,
-    },
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.find();
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
   });
-};
 
-exports.deleteOne = (Model) => async (req, res, next) => {
-  const doc = await Model.deleteOne({ slug: req.params.slug });
-  res.status(204).json({
-    status: "success",
-    data: {
-      data: doc,
-    },
+exports.getOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const query = Model.find({ slug: req.params.slug });
+    const doc = await query;
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
   });
-};
 
-exports.createOne = (Model) => async (req, res, next) => {
-  const doc = await Model.create(req.body);
-
-  res.status(201).json({
-    status: "success",
-    data: {
-      doc,
-    },
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.deleteOne({ slug: req.params.slug });
+    res.status(204).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
   });
-};
 
-exports.updateOne = (Model) => async (req, res, next) => {
-  let updateObject = req.body;
-  const doc = await Model.findOneAndUpdate(
-    { slug: req.params.slug },
-    { $set: updateObject },
-    { new: true, runValidators: true }
-  );
-  res.status(200).json({
-    status: "success",
-    data: {
-      data: doc,
-    },
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
   });
-};
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const updateObject = req.body;
+    const doc = await Model.findOneAndUpdate(
+      { slug: req.params.slug },
+      { $set: updateObject },
+      { new: true, runValidators: true }
+    );
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });

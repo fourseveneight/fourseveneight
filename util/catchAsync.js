3de4d08f87fff6d06e1b01ba61fb997a7547c786
Anish Sinha <anishsinha0128@gmail.com>
2021-07-24 21:25:27 -0400

    Modified catchAsync

--- util/catchAsync.js
+++ util/catchAsync.js
@@ -1,6 +1,8 @@
+// eslint-disable-next-line arrow-body-style
+const catchAsync = (fn) => {
+  return (req, res, next) => {
+    fn(req, res, next).catch(next);
+  };
+};
 
-const catchAsync = (fn)=>{
-   return (req, res, next)=>{
-        fn(req, res, next).catch(next);
-    }
-}
+module.exports = catchAsync;

f77eb6f91bbfab72b7e3e4fe2441f187f9411c69
Anish Sinha <anishsinha0128@gmail.com>
2021-07-26 23:24:54 -0400

    Modified auth.js to modularize a function used in userController

--- auth/auth.js
+++ auth/auth.js
@@ -6,6 +6,12 @@
  * **********************************************/
 
 module.exports = {
+  /**
+   * @param obj: object to be filtered
+   * @param  {...any} allowed: allowed fields
+   * @description: sanitizes an object
+   * @returns: sanitized object
+   */
   sanitizeBody: function (obj, ...allowed) {
     const sanitizedObject = {};
     Object.keys(obj).forEach((el) => {
@@ -15,6 +21,11 @@ module.exports = {
     });
     return sanitizedObject;
   },
+  /**
+   * @params request object, response object, next
+   * @description: checks user's active status
+   * @returns next function in the middleware stack if req.user.active is true
+   */
   checkStatus: function (req, res, next) {
     if (req.user.active === true) {
       return next();
@@ -26,6 +37,10 @@ module.exports = {
       },
     });
   },
+  /**
+   * @description Use to restrict certain routes to logged in users only
+   * @returns next() if the user is logged in.
+   */
   verifyAuthenticated: function (req, res, next) {
     if (req.isAuthenticated()) {
       return next();
@@ -37,6 +52,10 @@ module.exports = {
       },
     });
   },
+  /**
+   * @description Restricts certain routes by current user's role
+   * @returns next() if the user's role is admin or root
+   */
   restrictTo: function (req, res, next) {
     if (req.user.role === 'admin' || req.user.role === 'root') {
       return next();

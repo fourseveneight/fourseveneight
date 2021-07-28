ff29b50d6bff5428c60a1e7f7e4f553c5cce1c19
Anish Sinha <anishsinha0128@gmail.com>
2021-07-28 09:01:33 -0400

    All user routes have been tested and confirmed to work

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,23 +1,43 @@
+/**
+ * *User router
+ * @author root
+ * @description defines all user routes
+ * @datestarted July 24, 2021
+ * @datecompleted (v1) July 28, 2021
+ * *As of July 28, 2021, all routes below have been tested and confirmed to work
+ */
 const express = require('express'); //require express
 const userController = require('../controllers/userController'); //import controllers
+const {
+  //Auth functions to place on the middleware stack before protected resources
+  verifyAuthenticated,
+  restrictTo,
+  checkStatus,
+} = require('../auth/auth');
 
 const router = express.Router(); //initialize router
 
-router.route('/').get(userController.getAllUsers);
+router
+  .route('/') //* ✓
+  .get(verifyAuthenticated, restrictTo('root'), userController.getAllUsers);
 
 router
-  .route('/login') //on /login
+  .route('/login') //on /login //* ✓
   .get(userController.showLoginPage)
   .post(userController.login); //login function
 
 router
-  .route('/register')
+  .route('/register') //* ✓
   .get(userController.showRegisterPage)
   .post(userController.register);
 
-router.get('/logout', userController.logout);
-router.route('/recover').post(userController.recover);
-router.route('/reset/:token').post(userController.resetPassword);
-router.route('/confirm/:id').get(userController.confirmAccount);
+router
+  .route('/dashboard') //* ✓
+  .get(verifyAuthenticated, checkStatus, userController.showDashboard);
+
+router.get('/logout', userController.logout); //* ✓
+router.route('/recover').post(userController.recover); //* ✓
+router.route('/reset/:token').post(userController.resetPassword); //* ✓
+router.route('/confirm/:id').get(userController.confirmAccount); //* ✓
 
 module.exports = router;

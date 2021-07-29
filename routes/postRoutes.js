6899a39b070f0f19807ed65dd1f398c788b4b42f
Anish Sinha <anishsinha0128@gmail.com>
2021-07-29 19:24:46 -0400

    Added new routes and did more testing

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -5,22 +5,22 @@ const {
   verifyAuthenticated,
   checkStatus,
   verifyUserOriginalAuthorOfPost,
-} = require('../auth/auth');
+} = require('../auth/authFunctions');
 
 const router = express.Router();
 
 router
-  .route('/')
+  .route('/') //* ✓
   .get(postController.getAllPosts) //Don't need an account to view all posts
   .post(verifyAuthenticated, checkStatus, postController.createPost); //You do need an active account and to be logged in to create a post
 
-router.route('/hot').get(verifyAuthenticated, checkStatus, getHotPosts); //You need to be authenticated and active to get hot posts, since they are based on a user's tags
+router.route('/hot').get(verifyAuthenticated, checkStatus, getHotPosts); //* ✓ //You need to be authenticated and active to get hot posts, since they are based on a user's tags
 router
   .route('/new')
-  .get(postController.getNewPosts, postController.getAllPosts); //Get all new posts. You don't have to be logged in for this
+  .get(postController.getNewPosts, postController.getAllPosts); //* ✓ //Get all new posts. You don't have to be logged in for this
 router.route('/recommended').get(postController.getRecommendedPosts);
 
-router
+router //* ✓
   .route('/:slug')
   .get(postController.getPost)
   .patch(
@@ -29,9 +29,16 @@ router
     postController.updatePost
   )
   .delete(
+    //* ✓
     verifyAuthenticated,
     verifyUserOriginalAuthorOfPost,
     postController.deletePost
   ); //You don't need an account to view a post, but you needed to be logged in as user who originally created the post (or root) to edit or delete it
+
+router //* ✓
+  .route('/:slug/comments')
+  .post(verifyAuthenticated, checkStatus, postController.createNewCommentOnPost) //* ✓
+  .patch(verifyAuthenticated, checkStatus, postController.editCommentOnPost) //* ✓
+  .delete(verifyAuthenticated, checkStatus, postController.deleteCommentOnPost); //* ✓
 
 module.exports = router;
